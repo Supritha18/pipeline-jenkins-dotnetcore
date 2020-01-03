984a9fa7fb2a08b0045b1762f2b6656edb7901e9
dlromero <raiga1234@gmail.com>
2020-01-03 16:57:47 -0500

    fix solve

--- TDD/Calculadora.cs
+++ TDD/Calculadora.cs
@@ -6,6 +6,10 @@ namespace TDD
     {
         public double Suma(double input1, double input2)
         {
+            if (input2 == 0)
+            {
+                throw new System.ArgumentException("0 division error");
+            }
             return input1 + input2;
         }
 
@@ -30,11 +34,19 @@ namespace TDD
 
         public double Radicacion(double input1)
         {
+            if (input2 == 0)
+            {
+                throw new System.ArgumentException("0 division error");
+            }
             return Math.Sqrt(input1);
         }
 
         public double Potenciacion(double input1, double input2)
         {
+            if (input2 == 0)
+            {
+                throw new System.ArgumentException("0 division error");
+            }
             return Math.Pow(input1, input2);
         }
     }
